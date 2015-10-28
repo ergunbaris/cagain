10b261f81228e6f209f8d1071654810ef3aab1ae
bergun <baris.ergun@seagate.com>
2015-10-28 13:37:23 +0000

    tutorial: lets look at variable sizes

---
+++ tutorial/test_variable_sizes.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main(){
+    printf("int is of %lu bytes size\n", sizeof(int));
+    printf("long is of %lu bytes size\n", sizeof(long));
+    printf("float is of %lu bytes size\n", sizeof(float));
+    printf("double is of %lu bytes size\n", sizeof(double));
+    printf("char is of %lu bytes size\n", sizeof(char));
+    printf("short is of %lu bytes size\n", sizeof(short));
+}
