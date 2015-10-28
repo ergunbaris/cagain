a50e662798301aa8847951d693d23e8c655b1cf6
bergun <baris.ergun@seagate.com>
2015-10-28 12:40:13 +0000

    tutorial: printf place holders %o %x

---
+++ tutorial/test_printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int iVar = 88;
+    int iVarOctal = 077;
+    int iVarHex = 0xFE;
+    char cVar = 'c';
+    float fVar = 55.6F;
+    double dVar = 44.5;
+    printf("iVar=%d, cVar=%c, fVar=%6.1f, dvar=%.1f\n",iVar, cVar, fVar, dVar);
+    printf("iVarOctal=%o, iVarHex=0x%x, percent=%%\n",iVarOctal, iVarHex);
+}
