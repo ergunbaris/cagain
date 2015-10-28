d392f5815ee88cf680dbd4b32fec0a1ce23a2233
bergun <baris.ergun@seagate.com>
2015-10-28 11:10:26 +0000

    tutorial: local vars are initialized to default values when declared in C

---
+++ tutorial/test_loc_vars_init.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    int var, var2;  
+    // not like in java local variables are initialized to default values
+    printf("var=%d\n",var);
+    printf("var2=%d\n",var2);
+}
