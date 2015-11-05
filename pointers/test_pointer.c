2cbbd1729fe9ba881ce26943521ba0089d6ffe4a
bergun <baris.ergun@seagate.com>
2015-11-05 15:40:51 +0000

    pointers: printing the address of the pointers point to and address of the value which are all same

--- pointers/test_pointer.c
+++ pointers/test_pointer.c
@@ -5,6 +5,10 @@ int main(){
     int * p = &x;
     int * z = &x;
     int * y = p;
+    printf("p points to %p\n",p);
+    printf("z points to %p\n",z);
+    printf("y points to %p\n",y);
+    printf("x address is to %p\n",&x);
     *p = 9;
     printf("%d\n",x);
 }
