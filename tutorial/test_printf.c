3fac60c996d002c0c89d841bf003cc601311b462
bergun <baris.ergun@seagate.com>
2015-11-16 08:40:20 +0000

    test * sepcifier on printf

--- tutorial/test_printf.c
+++ tutorial/test_printf.c
@@ -9,4 +9,8 @@ int main(){
     double dVar = 44.5;
     printf("iVar=%d, cVar=%c, fVar=%6.1f, dvar=%.1f\n",iVar, cVar, fVar, dVar);
     printf("iVarOctal=%o, iVarHex=0x%x, percent=%%\n",iVarOctal, iVarHex);
+    int tab;
+    for(tab=0; tab < 100; tab+=4){
+        printf("%*s\n",tab,"test tab");
+    }
 }
